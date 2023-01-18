e0576b0f5651bcb10a26a0181314c981ba44b697
Bruno Sá <81045291+BrunoAGSa@users.noreply.github.com>
2023-01-18 19:23:32 +0000

    Praticamente Final Caraaaaalho!!!

--- Base de dados/Selects.sql
+++ Base de dados/Selects.sql
@@ -2,7 +2,7 @@ select * from Sequences;
 select * from Locus;
 select * from Genbank;
 select * from Features;
-select * from CDS;
+select * from CDS where Id_version_features = 'NC_013498.1';
 select * from Authors;
 select * from Pubmed_Info;
 select * from reference;
