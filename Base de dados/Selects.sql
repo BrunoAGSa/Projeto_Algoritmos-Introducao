f1307107279046b82549836b473254f49725648c
Bruno Sá <81045291+BrunoAGSa@users.noreply.github.com>
2023-01-18 13:47:19 +0000

    Após Bee

--- Base de dados/Selects.sql
+++ Base de dados/Selects.sql
@@ -2,4 +2,6 @@ select * from Sequences;
 select * from Locus;
 select * from Genbank;
 select * from Features;
-select * from CDS
+select * from CDS;
+select * from Authors;
+select * from Pubmed_Info;
