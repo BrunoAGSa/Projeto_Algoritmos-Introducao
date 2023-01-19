cb7bbe3d4a8e0c241e5dbd11ff0bafc0d8a86ff0
Bruno Sá <81045291+BrunoAGSa@users.noreply.github.com>
2023-01-19 16:25:33 +0000

    FInal

--- Base de dados/Selects.sql
+++ Base de dados/Selects.sql
@@ -2,10 +2,9 @@ select * from Sequences;
 select * from Locus;
 select * from Genbank;
 select * from Features;
-select * from CDS	;
+select * from CDS;
 select * from Authors;
 select * from reference_authors;
 select * from Pubmed_Info;
 select * from genbank_reference;
 select * from reference;
-
